aef2a3d41c7666e925d347af42ee257816434560
Daegon <romsssa@gmail.com>
2015-06-04 13:49:46 +0300

    Add UA language

---
+++ src/lang/ua/datatables.php
@@ -0,0 +1,16 @@
+<?php
+
+return array (
+    'lengthMenu' => '_MENU_ записів на сторінці',
+    'zeroRecords' => 'Записів не знайдено',
+    'info' => 'Показано з _START_ по _END_ записів з _TOTAL_',
+    'search' => 'Пошук: ',
+    'infoEmpty' => 'Не знайдено записів',
+    'infoFiltered' => ' - відфільтровано з _MAX_ записів',
+    'paginate' => [
+        'first' => 'У початок',
+        'last' => 'У кінець',
+        'next' => 'Наступна',
+        'previous' => 'Попередня',
+    ],
+);
